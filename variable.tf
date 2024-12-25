7aeeb7eb81cd3bbbb740bc8ab0ed5b489138e73b
Ankita Vakil <ankitasvakil@gmail.com>
2024-12-25 22:45:57 +0530

    cloudrun2

--- variable.tf
+++ variable.tf
@@ -7,6 +7,7 @@ variable "project_id" {
 variable "region" {
   description = "The GCP region"
   type        = string
+  default     = us-central1
 }
 
 variable "service_name" {
@@ -18,6 +19,7 @@ variable "service_name" {
 variable "location" {
   description = "The location for the Cloud Run service"
   type        = string
+  default     = us-central1
 }
 
 variable "image_url" {
