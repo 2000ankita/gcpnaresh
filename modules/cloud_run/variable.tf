9cb360ebbe7abea13b74218d5a20ebb5f28ca0e9
Ankita Vakil <ankitasvakil@gmail.com>
2024-12-25 22:43:20 +0530

    testing cloudrun1

--- modules/cloud_run/variable.tf
+++ modules/cloud_run/variable.tf
@@ -1,6 +1,7 @@
 variable "service_name" {
   description = "The name of the Cloud Run service"
   type        = string
+  default     = "cloudrun-service-1"
 }
 
 variable "location" {
@@ -8,14 +9,16 @@ variable "location" {
   type        = string
 }
 
-variable "project" {
+variable "project_id" {
   description = "The project ID where Cloud Run will be created"
   type        = string
+  default     = "gcptest-445516"
 }
 
 variable "image_url" {
   description = "The container image URL for the Cloud Run service"
   type        = string
+  default = "us-docker.pkg.dev/cloudrun/container/hello"
 }
 
 variable "memory" {
--- variable.tf
+++ variable.tf
@@ -1,12 +1,12 @@
 variable "project_id" {
   description = "The project ID where Cloud Run will be created"
   type        = string
+  default     = "gcptest-445516"
 }
 
 variable "region" {
   description = "The GCP region"
   type        = string
-  default     = "us-central1"
 }
 
 variable "service_name" {
@@ -18,12 +18,12 @@ variable "service_name" {
 variable "location" {
   description = "The location for the Cloud Run service"
   type        = string
-  default     = "us-central1"
 }
 
 variable "image_url" {
   description = "The container image URL for the Cloud Run service"
   type        = string
+  default = "us-docker.pkg.dev/cloudrun/container/hello"
 }
 
 variable "memory" {
