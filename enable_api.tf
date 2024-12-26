4fd13dd5780451d65bfec1da523d7aab59dddbc2
Ankita Vakil <ankitasvakil@gmail.com>
2024-12-26 22:52:06 +0530

    creating a compute engine19

--- enable_api.tf
+++ enable_api.tf
@@ -3,7 +3,14 @@ resource "google_project_service" "compute_engine_api" {
   service = "compute.googleapis.com"
 }
 
-resource "google_project_service" "enterprise_api" {
+resource "google_project_iam_member" "service_usage_admin" {
   project = var.project_id
-  service = "cloudenterprise.googleapis.com"
+  role    = "roles/serviceusage.serviceUsageAdmin"
+  member  = "serviceAccount:first-svc-account@gcptest-445516.iam.gserviceaccount.com"
+}
+
+resource "google_project_iam_member" "project_editor" {
+  project = var.project_id
+  role    = "roles/editor"
+  member  = "serviceAccount:first-svc-account@gcptest-445516.iam.gserviceaccount.com"
 }
